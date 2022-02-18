4c3c4d5a86bbf411dc8d75fe520b99fe1034056a
Diego Estrada <diego_estrada14@hotmail.com>
2022-02-18 00:25:24 -0600

    Se agregaron scripts para compilar y correr el servidor

---
+++ js/index.js
@@ -0,0 +1,31 @@
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+        /* Button script  */
+var myModal = document.getElementById('modalContacto');
+var btnContacto = document.getElementById('contactoBtn1');
+
+myModal.addEventListener('show.bs.modal', function (event) {
+    console.log('El modal se comenzó a mostrar.');
+    btnContacto.classList.remove('btn-outline-success');  
+    btnContacto.classList.add('btn-outline-warning');  
+    tnContacto.setAttribute('disabled', 'disabled')
+
+})
+
+myModal.addEventListener('shown.bs.modal', function (event) {
+    console.log('El modal se está mostrando.')
+})
+
+myModal.addEventListener('hide.bs.modal', function (event) {
+    console.log('El modal se está ocultando.')
+})
+
+myModal.addEventListener('hidden.bs.modal', function (event) {
+    console.log('El modal se está ocultó.')
+    btnContacto.classList.remove('btn-outline-warning');  
+    btnContacto.classList.add('btn-outline-success');  
+    btnContacto.removeAttribute('disabled')
+})
